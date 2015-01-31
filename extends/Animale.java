16eceae2fb02d869c98d8bb307f1d6d462ef5dad
kvegeta <alessandro.marongiu.82@gmail.com>
2015-01-31 18:06:43 +0100

    Created by Coding Ground Client

---
+++ extends/Animale.java
@@ -0,0 +1,21 @@
+public class Animale
+{
+    private String nome;
+    private String sesso;
+    
+    public Animale(String nome, String sesso)
+    {
+        this.nome = nome;
+        this. sesso = sesso;
+    }
+    
+    public String getNome()
+    {
+        return nome;    
+    }
+
+    public String getSesso()
+    {
+        return sesso;
+    }
+}
